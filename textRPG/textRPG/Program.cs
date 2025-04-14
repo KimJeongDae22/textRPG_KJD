6674cd9ed301e45f52049ed9308acfed1a2c600b
KimJeongDae22 <rlawjdeo0895@naver.com>
2025-04-14 16:58:11 +0900

    add - 상태 보기 기능 추가
     - 상태 보기 기능 추가

--- textRPG/textRPG/Program.cs
+++ textRPG/textRPG/Program.cs
@@ -1,8 +1,11 @@
-﻿Player player1;
+﻿using System;
 
+Player player1 = new Player();
+int num = 0;
 Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
 
 StartNaming();
+
 StartScene();
 void StartNaming()
 {
@@ -11,6 +14,7 @@ void StartNaming()
     if (inputName.Length >= 3 && inputName.Length <= 10)
     {
         player1.name = inputName;
+        Console.Clear();
     }
     else
     {
@@ -21,25 +25,26 @@ void StartNaming()
 }
 void StartScene()
 {
-    int num = 0;
     Console.WriteLine("환영합니다! {0} 님. 이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n", player1.name);
     Console.WriteLine("1. 상태보기 \n2. 인벤토리 \n3. 상점 \n");
-    Console.WriteLine("원하시는 행동을 입력해주세요");
+    Console.WriteLine("원하시는 행동을 입력해주세요.");
     string input = Console.ReadLine();
     num = int.Parse(input);
     if (num >= 1 && num <= 3)
     {
-        if (num == 1)   // 상태보기
+        switch (num)
         {
-
-        }
-        if (num == 2)   // 인벤토리
-        {
-
-        }
-        if (num == 3)   // 상점
-        {
-
+            case 1:
+                Console.Clear();
+                Information();
+                // 상태보기
+                break;
+            case 2:
+                // 인벤토리
+                break;
+            case 3:
+                // 상점
+                break;
         }
     }
     else
@@ -47,6 +52,34 @@ void StartScene()
         Console.Clear();
         Console.WriteLine("잘못된 입력입니다.\n");
         StartScene();
+    }
+}
+void Information()
+{
+    Console.WriteLine("-상태보기-\n캐릭터의 정보가 표시됩니다.\n");
+
+    Console.WriteLine("Lv.{0}", player1.level.ToString("D2"));
+    Console.WriteLine("이름 : {0} ( {1} )", player1.name, player1.jop);
+    Console.WriteLine("공격력 : {0}", player1.atkPower);
+    Console.WriteLine("방어력 : {0}", player1.armPower);
+    Console.WriteLine("체력 : {0}", player1.hp);
+    Console.WriteLine("골드 : {0}", player1.gold);
+
+    Console.WriteLine("\n0. 나가기");
+    Console.WriteLine("\n원하시는 행동을 입력해주세요.");
+    string input = Console.ReadLine();
+    int num = 1;
+    num = int.Parse(input);
+    if (num == 0)
+    {
+        Console.Clear();
+        StartScene();
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("잘못된 입력입니다.\n");
+        Information();
     }
 }
 struct Player()
@@ -59,6 +92,8 @@ struct Player()
     public int hp = 100;
     public int gold = 1500;
 }
+
+
 
 
 
--- textRPG/textRPG/obj/Debug/net8.0/textRPG.AssemblyInfo.cs
+++ textRPG/textRPG/obj/Debug/net8.0/textRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("textRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00855a9c154c65c94f43618fa5b66b989f86b77f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbb80063bf516cef790405e225ff9dea4342bb83")]
 [assembly: System.Reflection.AssemblyProductAttribute("textRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("textRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
